09e154f3f1d1265c5014f9affa0588f77f82a48e
Tess Huang <tesshuangxj@yahoo.com>
2020-02-21 09:49:55 -0800

    Added Trips page

--- src/pages/trips.js
+++ src/pages/trips.js
@@ -4,13 +4,13 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const SecondPage = () => (
+const Trips = () => (
   <Layout>
     <SEO title="Page two" />
-    <h1>Hi from the second page</h1>
+    <h1>Hi from trips page</h1>
     <p>Welcome to page 2</p>
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
 
-export default SecondPage
+export default Trips

5d939332e199550a67ae72938656ff876268c6ff
Tess Huang <tesshuangxj@yahoo.com>
2020-02-25 14:14:53 -0800

    Added ImageAPI component for image from the third-party api

---
+++ src/components/image-api.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import PropTypes from "prop-types"
+import Img from 'gatsby-image'
+
+const ImageAPI = ({ imageInfo }) => {
+  console.log(imageInfo);
+
+  return(
+    <Img src={imageInfo.image} alt={imageInfo.alt} />
+  )
+}
+
+ImageAPI.PropTypes = {
+  imageInfo: PropTypes.shape({
+    alt: PropTypes.string,
+    childImageSharp: PropTypes.object,
+    image: PropTypes.oneOfType([PropTypes.object, PropTypes.string]).isRequired,
+  })
+}
+
+export default ImageAPI

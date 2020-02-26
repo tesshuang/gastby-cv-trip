470e0f6a626b797ed1d6e3ce61aa3aea7438e360
Tess Huang <tesshuangxj@yahoo.com>
2020-02-25 16:17:20 -0800

    Updated image query to be fluid

--- src/components/image-api.js
+++ src/components/image-api.js
@@ -4,13 +4,24 @@ import Img from 'gatsby-image'
 
 const ImageAPI = ({ imageInfo }) => {
   console.log(imageInfo);
+  const { alt = '', childImageSharp, image } = imageInfo
+  if (!!image && !!image.childImageSharp) {
+    return (
+      <Img fluid={image.childImageSharp.fluid} alt={alt} />
+    )
+  }
 
-  return(
-    <Img src={imageInfo.image} alt={imageInfo.alt} />
-  )
+  if (!!childImageSharp) {
+    return <Img fluid={childImageSharp.fluid} alt={alt} />
+  }
+
+  if (!!image && typeof image === 'string')
+    return <img src={image} alt={alt} />
+
+  return null
 }
 
-ImageAPI.PropTypes = {
+ImageAPI.propTypes = {
   imageInfo: PropTypes.shape({
     alt: PropTypes.string,
     childImageSharp: PropTypes.object,
--- src/templates/trip-graph.js
+++ src/templates/trip-graph.js
@@ -3,6 +3,7 @@ import { graphql } from 'gatsby'
 
 import Layout from '../components/layout'
 import SEO from '../components/seo'
+import ImageAPI from '../components/image-api'
 
 export default ({ data }) => {
   console.log(data);
@@ -12,6 +13,12 @@ export default ({ data }) => {
       <SEO title="" />
       <h1>Hi from Trip Graph Template Page</h1>
       <h2>{trip.name}</h2>
+      <ImageAPI 
+        imageInfo={{
+          image: trip.imageFile,
+          alt: `Feature image for ${trip.name}`,
+        }}
+      />
       <p>Duration: {trip.duration} days</p>
     </Layout>
   )
@@ -26,8 +33,8 @@ export const query = graphql`
       slug
       imageFile {
       childImageSharp {
-        fixed(width: 300) {
-          ...GatsbyImageSharpFixed
+        fluid(maxWidth: 640) {
+          ...GatsbyImageSharpFluid
         }
       }
     }

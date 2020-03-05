b62714b889d59237859ff1f373f81675c7dd5fd4
Tess Huang <tesshuangxj@yahoo.com>
2020-03-04 16:52:57 -0800

    Updated content in the template trip page

--- src/templates/trip-bms.js
+++ src/templates/trip-bms.js
@@ -6,11 +6,30 @@ import SEO from '../components/seo'
 
 const BMSData = ({ data }) => {
   console.log(data);
+  const trip = data.bmsTrip;
   return (
     <Layout>
-      <SEO title="" />
+      <SEO title={trip.name} />
+      <h1>{trip.name}</h1>
+      <h3>Overview</h3>
+      <div dangerouslySetInnerHTML={{__html: trip.description}}/>
+      <h3>Marketing highlights</h3>
+      <div dangerouslySetInnerHTML={{ __html: trip.marketing_highlights }} />
+      <h3>What's Included</h3>
+      <div dangerouslySetInnerHTML={{__html: trip.whats_included}}/>
     </Layout>
   )
 }
+
+export const query = graphql`
+  query BMSTripQuery($code: String) {
+    bmsTrip(code: {eq: $code}) {
+      name
+      marketing_highlights
+      description
+      whats_included
+    }
+  }
+`
 
 export default BMSData;
